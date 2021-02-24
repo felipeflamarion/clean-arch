9a9f3428e969e5fb60e85efab5979bbdd0d6ac48
Felipe Flamarion <felipeflamari@gmail.com>
2021-02-24 00:58:01 -0300

    Add new tables creation

--- db/clean_arch.sql
+++ db/clean_arch.sql
@@ -1,20 +1,47 @@
 DROP TABLE IF EXISTS todolist_tickets;
+DROP TABLE IF EXISTS todolist_persons;
+DROP TABLE IF EXISTS todolist_board_columns;
 DROP TABLE IF EXISTS todolist_boards;
 
 CREATE TABLE todolist_boards (
     id SERIAL,
     title VARCHAR(128) NOT NULL,
-    creation_date TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
     CONSTRAINT pk_todolist_boards PRIMARY KEY (id)
+);
+
+CREATE TABLE todolist_board_columns (
+    id SERIAL,
+    board_id INT NOT NULL,
+    name VARCHAR(32) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    CONSTRAINT pk_todolist_boards PRIMARY KEY (id),
+    CONSTRAINT fk_todolist_board_columsn_todolist_boards FOREIGN KEY (board_id) REFERENCES todolist_boards(id)
+);
+
+CREATE TABLE todolist_persons (
+    id SERIAL,
+    username VARCHAR(32) NOT NULL,
+    display_name VARCHAR(128) NOT NULL,
+    email VARCHAR(128) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    CONSTRAINT pk_todolist_persons PRIMARY KEY (id)
 );
 
 CREATE TABLE todolist_tickets (
     id SERIAL,
-    board_id INT NOT NULL,
+    board_column_id INT DEFAULT NULL,
+    person_id INT DEFAULT NULL,
     title VARCHAR(128) NOT NULL,
     description TEXT DEFAULT NULL,
+    priority INT DEFAULT 0,
     labels TEXT DEFAULT NULL,
-    creation_date TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
     CONSTRAINT pk_todolist_tickets PRIMARY KEY (id),
-    CONSTRAINT fk_todolist_tickets_todolist_boards FOREIGN KEY (board_id) REFERENCES todolist_boards(id)
+    CONSTRAINT fk_todolist_tickets_todolist_board_columns FOREIGN KEY (board_column_id) REFERENCES todolist_board_columns(id),
+    CONSTRAINT fk_todolist_tickets_todolist_persons FOREIGN KEY (person_id) REFERENCES todolist_persons(id)
 );
